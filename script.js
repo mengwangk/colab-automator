8fc6de19fcf74eb52a5eb1ff22e28d62e8e561cd
Meng Wang <mengwangk@gmail.com>
2021-02-28 23:42:29 +0800

    Initial commit

--- script.js
+++ script.js
@@ -9,4 +9,3 @@ function ClickRefresh(){
     document.querySelector("body > div.notebook-vertical.colab-left-pane-open > div.notebook-horizontal > colab-left-pane > colab-resizer > div.resizer-contents > iron-pages > colab-file-browser > colab-file-tree > div.file-tree-buttons > div > colab-toolbar-button:nth-child(2) > paper-button > span").click() 
 }
 setInterval(ClickRefresh,30000)
-
